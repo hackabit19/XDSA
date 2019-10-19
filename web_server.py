2c8a82cfa0fa66c5ae20633fb6c160dfbcb2b39d
Deepak Hirawat <bit.deepak99@gmail.com>
2019-10-19 11:12:28 +0530

    Integrated ssd_mobilenet, now its is V 2.0

--- web_server.py
+++ web_server.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template, Response , json
 import cv2
 from captionbot import CaptionBot
+import os
+import time
+import tarfile
+import glob
+import six.moves.urllib as urllib
+from tqdm import tqdm
+import tensorflow as tf
+from ssd_mobilenet_utils import *
 import numpy as np
 
 app = Flask(__name__)
+c = CaptionBot()
 frame = None
+interpreter = tf.lite.Interpreter(model_path="model_data/ssdlite_mobilenet_v2.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+class_names = read_classes('model_data/coco_classes.txt')
+colors = generate_colors(class_names)
+# real_time_object_detection(interpreter, colors)
 
 @app.route('/')
 @app.route('/index')
@@ -15,12 +31,42 @@ def index():
 def home():
     return render_template('home.html')
 
+def run_detection(image, interpreter):
+    interpreter.set_tensor(input_details[0]['index'], image)
+    interpreter.invoke()
+
+    boxes = interpreter.get_tensor(output_details[0]['index'])
+    classes = interpreter.get_tensor(output_details[1]['index'])
+    scores = interpreter.get_tensor(output_details[2]['index'])
+    num = interpreter.get_tensor(output_details[3]['index'])
+
+    boxes, scores, classes = np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes + 1).astype(np.int32)
+    out_scores, out_boxes, out_classes = non_max_suppression(scores, boxes, classes)
+
+    # Print predictions info
+    #print('Found {} boxes for {}'.format(len(out_boxes), 'images/dog.jpg'))
+
+    return out_scores, out_boxes, out_classes
+
 def gen():
     cap = cv2.VideoCapture(1)
     global frame
-    while True:
+    global interpreter
+    global colors
+    while cap.isOpened():
+        start = time.time()
         ret ,frame =  cap.read()
         if ret == True:
+            cv2.imwrite('image.jpg' , frame)
+            image_data = preprocess_image_for_tflite(frame, model_image_size=300)
+            out_scores, out_boxes, out_classes = run_detection(image_data, interpreter)
+            result = draw_boxes(frame, out_scores, out_boxes, out_classes, class_names, colors)
+            end = time.time()
+
+            t = end - start
+            fps  = "Fps: {:.2f}".format(1 / t)
+            cv2.putText(result, fps, (10, 30),
+		                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             flag, encodedImage = cv2.imencode(".jpg" , frame)
             if not flag:
                 continue
@@ -37,10 +83,7 @@ def video_feed():
 
 @app.route('/generate_caption')
 def generate_caption():
-    c = CaptionBot()
     print("Generating Caption...")
-    global frame
-    cv2.imwrite('image.jpg' , frame)
     # caption = c.file_caption('/home/aditya/Hack-a-bit2019/' + 'image.jpg')
     caption = c.file_caption("C:/Users/Bharat/Desktop/Hack-A-Bit 2019/image.jpg")
     print(caption)
